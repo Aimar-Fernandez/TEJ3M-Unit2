c7aed31ab76338688eb56d804505d9c21556886e
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2026-02-18 14:19:11 -0500

    Added 2-02 program

---
+++ delayedBlink.ino
@@ -0,0 +1,20 @@
+// Created by Aimar Fernandez
+// Created on Feb 2026
+// This program delays the arduino blink time longer
+
+int blinkTime = 1000;
+
+void setup() {
+  // the setup function runs once when you press reset or power the board
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // the loop function runs over and over again forever
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(blinkTime);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(blinkTime);                 
+
+  blinkTime = blinkTime + 500;
+}
